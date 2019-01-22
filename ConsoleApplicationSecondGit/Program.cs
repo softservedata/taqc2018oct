67b0585c4b45646bd4e21ab3d35fc9763982282c
bebebe <hahaha12@gmail.com>
2019-01-22 11:20:52 +0200

    Example Web Service

--- ConsoleApplicationSecondGit/Program.cs
+++ ConsoleApplicationSecondGit/Program.cs
@@ -10,7 +10,16 @@ namespace ConsoleApplicationSecondGit
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Start");
             // Testing Web Services
+            ServiceReference.CalcSEIClient calc = new ServiceReference.CalcSEIClient();
+            //int expected = 7;
+            int actual = calc.add(3, 4);
+            Console.WriteLine("actual = " + actual);
+            //
+            Console.WriteLine("5+7 = " + calc.add(5, 7));
+            Console.WriteLine("Answer = " + calc.hello("Ivan"));
+            Console.WriteLine("Done");
         }
     }
 }
