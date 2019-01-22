78a7ab5b73971a68b2883e9245bb2ec303515593
bebebe <hahaha12@gmail.com>
2019-01-22 12:06:00 +0200

    Example Rest

--- ConsoleApplicationSecondGit/Program.cs
+++ ConsoleApplicationSecondGit/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace ConsoleApplicationSecondGit
     {
         public static void Main(string[] args)
         {
+            string url = "http://localhost:8080/";
+            var client = new RestClient(url);
+            //var request = new RestRequest("/tokenlifetime", Method.GET);
+            //
+            var request = new RestRequest("/login", Method.POST);
+            request.AddParameter("name", "admin");
+            request.AddParameter("password", "qwerty");
+            //
+            IRestResponse response = client.Execute(request);
+            //
+            var content = response.Content;
+            Console.WriteLine("content: " + content);
         }
     }
 }
