a20924492aa0aa8710d7a53faca472319eb27208
bebebe <hahaha12@gmail.com>
2019-01-28 20:02:37 +0200

    Simple Test

---
+++ UnitTestProjectSecondA1/SimpleTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Firefox;
+
+namespace UnitTestProjectSecondA1
+{
+    [TestFixture]
+    public class SimpleTest
+    {
+        [Test]
+        public void CheckLogin()
+        {
+            // Precondition
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20); // by default 0
+            driver.Navigate().GoToUrl("https://softserve.academy/");
+            Thread.Sleep(1000); // For Presentation ONLY
+            //
+            // Steps
+            driver.FindElement(By.LinkText("Log in")).Click();
+            Thread.Sleep(1000); // For Presentation ONLY
+            //
+            driver.FindElement(By.Id("username")).Click();
+            driver.FindElement(By.Id("username")).Clear();
+            driver.FindElement(By.Id("username")).SendKeys("Hello Temp");
+            Thread.Sleep(2000); // For Presentation ONLY
+            //
+            //
+            driver.Navigate().Refresh();
+            Thread.Sleep(2000); // For Presentation ONLY
+            //
+            driver.FindElement(By.Id("username")).SendKeys(" new Hello");
+            Thread.Sleep(2000); // For Presentation ONLY
+            //
+            driver.FindElement(By.Id("password")).Click();
+            driver.FindElement(By.Id("password")).Clear();
+            driver.FindElement(By.Id("password")).SendKeys("qwerty");
+            Thread.Sleep(2000); // For Presentation ONLY
+            //
+            driver.Quit();
+        }
+
+    }
+}
