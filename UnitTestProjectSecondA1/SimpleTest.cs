c93f758583c6e11788717e070fde14e722abe9cb
bebebe <hahaha@gmail.com>
2019-01-29 10:57:54 +0200

    CheckTime Test Rest Example

--- UnitTestProjectSecondA1/SimpleTest.cs
+++ UnitTestProjectSecondA1/SimpleTest.cs
@@ -10,12 +10,30 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Firefox;
 using RestSharp;
+using RestSharp.Serialization.Json;
 
 namespace UnitTestProjectSecondA1
 {
+    public class RestResult
+    {
+        public string content { get; set; }
+
+        public RestResult()
+        {
+            content = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return content;
+        }
+    }
+
     [TestFixture]
     public class SimpleTest
     {
+        private string tokenAdmin;
+
         //[Test]
         public void CheckLogin()
         {
@@ -49,7 +67,7 @@ namespace UnitTestProjectSecondA1
             driver.Quit();
         }
 
-        [Test]
+        //[Test]
         public void CheckRest()
         {
             // RestClient
@@ -65,7 +83,8 @@ namespace UnitTestProjectSecondA1
             //var request = new RestRequest("item/125", Method.PUT);
             //var request = new RestRequest("item/125", Method.DELETE);
             //var request = new RestRequest("/users", Method.GET);
-            var request = new RestRequest("/login", Method.POST);
+            //var request = new RestRequest("/login", Method.POST);
+            var request = new RestRequest(Method.GET);
             //
             //request.RequestFormat = DataFormat.Json;
             //
@@ -116,6 +135,96 @@ namespace UnitTestProjectSecondA1
             // asyncHandle.Abort();
             //
             Console.WriteLine("done");
+        }
+
+        //[Test]
+        public void CheckTime()
+        {
+            string url = "http://localhost:8080/";
+            var client = new RestClient(url);
+            //
+            var request = new RestRequest("/login", Method.POST);
+            request.AddParameter("name", "admin");
+            request.AddParameter("password", "qwerty");
+            IRestResponse response = client.Execute(request);
+            //
+            JsonDeserializer deserial = new JsonDeserializer();
+            string adminToken = deserial.Deserialize<RestResult>(response).content;
+            Assert.IsTrue(adminToken.Length > 0, "Login Error");
+            //
+            request = new RestRequest("/tokenlifetime", Method.GET);
+            response = client.Execute(request);
+            string time = deserial.Deserialize<RestResult>(response).content;
+            Assert.AreEqual("300000", time, "Time Error");
+            //
+            request = new RestRequest("/tokenlifetime", Method.PUT);
+            request.AddParameter("token", adminToken);
+            request.AddParameter("time", "800000");
+            response = client.Execute(request);
+            string result = deserial.Deserialize<RestResult>(response).content;
+            StringAssert.AreEqualIgnoringCase("true", result, "Update Time Error");
+            //Assert.AreEqual("true", result, "Update Time Error");
+            //
+            request = new RestRequest("/logout", Method.POST);
+            request.AddParameter("name", "admin");
+            request.AddParameter("token", adminToken);
+            response = client.Execute(request);
+            result = deserial.Deserialize<RestResult>(response).content;
+            StringAssert.AreEqualIgnoringCase("true", result, "Logout Error");
+        }
+
+        [Test, Order(1)]
+        public void VerifyLogin()
+        {
+            var client = new RestClient("http://localhost:8080/login");
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("name", "admin");
+            request.AddParameter("password", "qwerty");
+            IRestResponse response = client.Execute(request);
+            //
+            JsonDeserializer deserial = new JsonDeserializer();
+            tokenAdmin = deserial.Deserialize<RestResult>(response).content;
+            Assert.IsTrue(tokenAdmin.Length > 0, "Login Error");
+        }
+
+        [Test, Order(2)]
+        public void VerifyTime()
+        {
+            var client = new RestClient("http://localhost:8080/tokenlifetime");
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            //
+            JsonDeserializer deserial = new JsonDeserializer();
+            string time = deserial.Deserialize<RestResult>(response).content;
+            Assert.AreEqual("300000", time, "Time Error");
+        }
+
+        [Test, Order(3)]
+        public void VerifyUpdateTime()
+        {
+            var client = new RestClient("http://localhost:8080/tokenlifetime");
+            var request = new RestRequest(Method.PUT);
+            request.AddParameter("token", tokenAdmin);
+            request.AddParameter("time", "800000");
+            IRestResponse response = client.Execute(request);
+            //
+            JsonDeserializer deserial = new JsonDeserializer();
+            string result = deserial.Deserialize<RestResult>(response).content;
+            StringAssert.AreEqualIgnoringCase("true", result, "Update Time Error");
+        }
+
+        [Test, Order(4)]
+        public void VerifyLogout()
+        {
+            var client = new RestClient("http://localhost:8080/logout");
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("name", "admin");
+            request.AddParameter("token", tokenAdmin);
+            IRestResponse response = client.Execute(request);
+            //
+            JsonDeserializer deserial = new JsonDeserializer();
+            string result = deserial.Deserialize<RestResult>(response).content;
+            StringAssert.AreEqualIgnoringCase("true", result, "Logout Error");
         }
     }
 }
